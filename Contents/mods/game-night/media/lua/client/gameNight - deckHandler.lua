0517bcfe8c81556b8cfb02fcb6bbe103efb9dbb1
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-02-23 19:24:01 -0500

    Update gameNight - deckHandler.lua

--- Contents/mods/game-night/media/lua/client/gameNight - deckHandler.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - deckHandler.lua
@@ -1,13 +1,36 @@
 --local card = {}--["name"] = value
 --local deck = {}--["ID"]=card(?)
 
+
 local deckHandler = {}
+
+
+function deckHandler.getAndOrSetDeck(deckItem)
+    local deckData = deckItem:getModData()["cardDeck"]
+
+    if not deckData then
+        ---initiate deck
+    end
+
+    return deckData
+end
+
 
 ---@param deckItem InventoryItem
 function deckHandler.addCard(deckItem)
-    local deckData = deckItem:hasModData() and deckItem:getModData()["cardDeck"]
-    if not deckData then return end
+    local deck = deckHandler.getAndOrSetDeck(deckItem)
+    if not deck then return end
 
 end
 
+
+function deckHandler.shuffle(deckItem)
+
+    local deck = deckHandler.getAndOrSetDeck(deckItem)
+
+    for origIndex = #deck, 2, -1 do
+        local shuffledIndex = ZombRand(origIndex)+1
+        deck[origIndex], deck[shuffledIndex] = deck[shuffledIndex], deck[origIndex]
+    end
+end
 
