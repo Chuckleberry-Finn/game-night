8e49408f70843439fd5c39be8f687f899e8ac61a
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-02-23 20:36:04 -0500

    Update gameNight - deckHandler.lua

--- Contents/mods/game-night/media/lua/client/gameNight - deckHandler.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - deckHandler.lua
@@ -4,29 +4,26 @@
 
 local deckHandler = {}
 
-
+---@param deckItem IsoObject
 function deckHandler.getAndOrSetDeck(deckItem)
-    local deckData = deckItem:getModData()["cardDeck"]
-
-    if not deckData then
-        ---initiate deck
-    end
-
+    deckItem:getModData()["gameNight_cardDeck"] = deckItem:getModData()["gameNight_cardDeck"] or {}
+    local deckData = deckItem:getModData()["gameNight_cardDeck"]
     return deckData
 end
 
 
 ---@param deckItem InventoryItem
-function deckHandler.addCard(deckItem)
+function deckHandler.addCard(deckItem, card)
     local deck = deckHandler.getAndOrSetDeck(deckItem)
-    if not deck then return end
-
+    if not deck then return print("ERROR: Unable to register deck: "..tostring(deckItem)) end
+    table.insert(deck, card)
 end
 
 
 function deckHandler.shuffle(deckItem)
 
     local deck = deckHandler.getAndOrSetDeck(deckItem)
+    if not deck then return print("ERROR: Unable to register deck: "..tostring(deckItem)) end
 
     for origIndex = #deck, 2, -1 do
         local shuffledIndex = ZombRand(origIndex)+1
