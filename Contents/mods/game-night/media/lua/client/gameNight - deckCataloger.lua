984a230b4aa47c32e9021fe7934c54065210b43e
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-02-25 07:02:19 -0500

    adds applyDeck

--- Contents/mods/game-night/media/lua/client/gameNight - deckCataloger.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - deckCataloger.lua
@@ -6,4 +6,10 @@ function deckCataloger.addDeck(name, cards)
     deckCataloger.catalogues[name] = cards
 end
 
+---@param item InventoryItem
+function deckCataloger.applyDeck(item)
+    local deck = deckCataloger.catalogues[item:getType()]
+    if deck then item:getModData()["gameNight_cardDeck"] = copyTable(deck) end
+end
+
 return deckCataloger
