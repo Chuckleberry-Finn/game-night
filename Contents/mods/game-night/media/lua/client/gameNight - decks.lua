9c9efdb470adee39ea21a60975cf81a97667e127
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-02-25 07:02:31 -0500

    adds UNO

--- Contents/mods/game-night/media/lua/client/gameNight - decks.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - decks.lua
@@ -1,15 +1,46 @@
+--- For anyone looking to make a sub-mod:
+-- This file is all that you should need.
+
+---First require this file so that the cataloger module can be called on.
 local deckCataloger = require "gameNight - deckCataloger"
 
+--- Examples of defining a table
+-- this example is overly complicated as it pieces together a table for the sake of typing up a large list
+-- technically you just need a table of strings corresponding to textures/names for items
+
+
+--- Basic Playing Cards
 --Joker red and black
-local playingCards = {"JokerR","JokerB"}
+local playingCards = {}
+playingCards.cards = {"JokerR","JokerB"}
 --Hearts, Clubs, Diamonds, Spades
 playingCards.suites = {"H","C","D","S"}
 playingCards.values = {"2","3","4","5","6","7","8","9","10","J","K","Q","A"}
----Parse through suites and values to generate playingCards - this is not 'technically' required but I didn't see a point in typing out a list of 52 entries
+---Parse through suites and values to generate playingCards
+-- this is not 'technically' required but I didn't see a point in typing out a list of 52 entries
 for _,s in pairs(playingCards.suites) do
     for _,v in pairs(playingCards.values) do
-        table.insert(playingCards, v..s)
+        table.insert(playingCards.cards, v..s)
     end
 end
+deckCataloger.AddDeck("CardDeck", playingCards.cards)
 
-deckCataloger.AddDeck("playing cards", playingCards)
+
+--- UNO
+-- (19) Red, Blue, Green, Yellow – 0, 1 to 9 (2x)
+-- (8) Skip, Reverse, Draw2 – 2 cards of each color
+-- (8) Black – 4 Wild cards and 4 Wild Draw 4 cards
+local unoCards = {}
+unoCards.cards = {"R0","G0","B0","Y0","wild","wild","wild","wild","wildDraw4","wildDraw4","wildDraw4","wildDraw4"}
+
+unoCards.suites = {"R","G","B","Y"}
+unoCards.values = {"1","2","3","4","5","6","7","8","9","skip","reverse","draw2"}
+
+for i=1, 2 do --Two sets of 1-9, 0s are single
+    for _,s in pairs(unoCards.suites) do
+        for _,v in pairs(unoCards.values) do
+            table.insert(unoCards.cards, s..v)
+        end
+    end
+end
+deckCataloger.AddDeck("UnoDeck", unoCards.cards)
