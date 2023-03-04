55d7ae6e9c2e1b993b99ba2bdfe10ab06f41cd55
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-03-04 11:36:13 -0500

    adds context menus

---
+++ Contents/mods/game-night/media/lua/client/gameNight - deckContextMenu.lua
@@ -0,0 +1,24 @@
+require "ISUI/ISInventoryPaneContextMenu"
+local deckCataloger = require "gameNight - deckCataloger"
+local deckActionHandler = require "gameNight - deckActionHandler"
+
+local deckContext = {}
+
+function deckContext.addContext(player, context, items)
+    for _, v in ipairs(items) do
+
+        local item = v
+        if not instanceof(v, "InventoryItem") then item = v.items[1] end
+
+        local deck = deckActionHandler.getDeck(item)
+
+        if deck and #deck>1 then
+            context:addOption(getText("IGUI_drawCard"), item, deckActionHandler.drawCard, player)
+            context:addOption(getText("IGUI_drawRandCard"), item, deckActionHandler.drawRandCard, player)
+            context:addOption(getText("IGUI_shuffleCards"), item, deckActionHandler.shuffleCards, player)
+            break
+        end
+    end
+end
+
+Events.OnPreFillInventoryObjectContextMenu.Add(deckContext.addContext)
