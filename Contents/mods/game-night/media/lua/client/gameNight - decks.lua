58f7b1f639db63beb67c94aa1ca08101a2494fe4
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2023-03-09 22:11:34 -0500

    proper named cards

--- Contents/mods/game-night/media/lua/client/gameNight - decks.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - decks.lua
@@ -12,15 +12,15 @@ local deckCataloger = require "gameNight - deckCataloger"
 --- Basic Playing Cards
 --Joker red and black
 local playingCards = {}
-playingCards.cards = {"JokerR","JokerB"}
+playingCards.cards = {"Red Joker","Black Joker"}
 --Hearts, Clubs, Diamonds, Spades
-playingCards.suites = {"H","C","D","S"}
-playingCards.values = {"2","3","4","5","6","7","8","9","10","J","K","Q","A"}
+playingCards.suites = {"Hearts","Clubs","Diamonds","Spades"}
+playingCards.values = {"2","3","4","5","6","7","8","9","10","Jack","King","Queen","Ace"}
 ---Parse through suites and values to generate playingCards
 -- this is not 'technically' required but I didn't see a point in typing out a list of 52 entries
 for _,s in pairs(playingCards.suites) do
     for _,v in pairs(playingCards.values) do
-        table.insert(playingCards.cards, v..s)
+        table.insert(playingCards.cards, v.." of "..s)
     end
 end
 deckCataloger.addDeck("CardDeck", playingCards.cards)
@@ -31,15 +31,15 @@ deckCataloger.addDeck("CardDeck", playingCards.cards)
 -- (8) Skip, Reverse, Draw2 – 2 cards of each color
 -- (8) Black – 4 Wild cards and 4 Wild Draw 4 cards
 local unoCards = {}
-unoCards.cards = {"R0","G0","B0","Y0","wild","wild","wild","wild","wildDraw4","wildDraw4","wildDraw4","wildDraw4"}
+unoCards.cards = {"Red 0","Green 0","Blue 0","Yellow 0","Wild","Wild","Wild","Wild","Wild Draw 4","Wild Draw 4","Wild Draw 4","Wild Draw 4"}
 
-unoCards.suites = {"R","G","B","Y"}
+unoCards.suites = {"Red","Green","Blue","Yellow"}
 unoCards.values = {"1","2","3","4","5","6","7","8","9","skip","reverse","draw2"}
 
 for i=1, 2 do --Two sets of 1-9, 0s are single
     for _,s in pairs(unoCards.suites) do
         for _,v in pairs(unoCards.values) do
-            table.insert(unoCards.cards, s..v)
+            table.insert(unoCards.cards, s.." "..v)
         end
     end
 end
