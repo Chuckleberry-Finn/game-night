980db5ce1a4c50724cb6daaa48440c77ccc153c1
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-08-28 17:26:01 -0400

    Update gameNight - window.lua

--- Contents/mods/game-night/media/lua/client/gameNight - window.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - window.lua
@@ -172,7 +172,7 @@ function gameNightWindow:render()
     gameNightWindow.cursorH = gameNightWindow.cursorH or gameNightWindow.cursor:getHeight()
 
     for username,data in pairs(self.cursorDraws) do
-        self:drawTexture(gameNightWindow.cursor, data.x-gameNightWindow.cursorW, data.x-gameNightWindow.cursorW, 1, data.r, data.g, data.b)
+        self:drawTexture(gameNightWindow.cursor, data.x-gameNightWindow.cursorW, data.y-gameNightWindow.cursorH, 1, data.r, data.g, data.b)
         self:drawText(username, data.x, data.y, data.r, data.g, data.b, 1, UIFont.NewSmall)
     end
     self.cursorDraws = {}
