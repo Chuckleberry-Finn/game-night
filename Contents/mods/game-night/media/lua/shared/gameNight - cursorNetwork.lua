3d49ce42c9cd429c0b112c8ed42c2b09c1713521
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-08-29 21:07:52 -0400

    Update gameNight - cursorNetwork.lua

--- Contents/mods/game-night/media/lua/shared/gameNight - cursorNetwork.lua
+++ Contents/mods/game-night/media/lua/shared/gameNight - cursorNetwork.lua
@@ -1,7 +1,7 @@
 if isServer() then
     local function onClientCommand(_module, _command, _player, _data)
         if _module ~= "gameNightCursor" then return end
-        if _command == "update" then sendServerCommand(_player, _module, _command, _data) end
+        if _command == "update" then sendServerCommand(_module, _command, _data) end
     end
     Events.OnClientCommand.Add(onClientCommand)--what the server gets from the client
 end
