1d6b76fc1d5900b69d973122b3aea07d4c8d781c
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-09-10 10:33:10 -0400

    Update gameNight - implementCatan.lua

--- Contents/mods/game-night - Catan/media/lua/client/gameNight - implementCatan.lua
+++ Contents/mods/game-night - Catan/media/lua/client/gameNight - implementCatan.lua
@@ -1,44 +1,45 @@
 --- For anyone looking to make a sub-mod:
---TERMS: CARDS AND GAME PIECES/BOARDS
+--- ! SEE: `gameNight - implementUno` for a simpler example on adding decks.
+--- CATAN includes 'game pieces' rather than *just* cards - scroll to the bottom for more info on those.
 
----First require this file so that the cataloger module can be called on.
 local applyItemDetails = require "gameNight - applyItemDetails"
 
---- Examples of defining a table
--- this example is overly complicated as it pieces together a table for the sake of typing up a large list
--- technically you just need a table of strings corresponding to textures/names for items
-
-
---- CATAN RESOURCES
+-- CARDS
+--- CATAN RESOURCES DECK
 local CatanResourceDeck = {}
 CatanResourceDeck.types = {"Brick","Stone","Wood","Wheat","Sheep"}
 CatanResourceDeck.cards = {}
+
 for _,s in pairs(CatanResourceDeck.types) do for i=1, 19 do table.insert(CatanResourceDeck.cards, s) end end
 applyItemDetails.addDeck("CatanResourceDeck", CatanResourceDeck.cards)
 
-
---- CATAN DEVELOPMENTS
+--- CATAN DEVELOPMENTS DECK
 local CatanDevelopmentDeck = {}
+
+--Five (5) Victory Point Cards (Chapel, Library, Market, Palace, University).
+CatanDevelopmentDeck.cards = {"Chapel", "Library", "Market", "Palace", "University"}
+
 --Fourteen (14) Knight Cards.
 --Six (6) Progress Cards (2 x Monopoly, 2 x Road Building, 2 x Year of Plenty).
 CatanDevelopmentDeck.types = {"Knight","Monopoly","Road Building","Year of Plenty"}
 CatanDevelopmentDeck.count = {14,2,2,2}
---Five (5) Victory Point Cards (Chapel, Library, Market, Palace, University).
-CatanDevelopmentDeck.cards = {"Chapel", "Library", "Market", "Palace", "University"}
 
-for i,s in pairs(CatanDevelopmentDeck.types) do
-    for ii=1, i do
-        table.insert(CatanDevelopmentDeck.cards, s)
-    end
-end
+for i,s in pairs(CatanDevelopmentDeck.types) do for ii=1, i do table.insert(CatanDevelopmentDeck.cards, s) end end
 applyItemDetails.addDeck("CatanDevelopmentDeck", CatanDevelopmentDeck.cards)
 
 
-
----Identify game pieces by type
+-- GAME PIECES / GAME BOARD
+---First require this file so that the gamePieceAndBoardHandler module can be called on.
 local gamePieceAndBoardHandler = require "gameNight - gamePieceAndBoardHandler"
 
-gamePieceAndBoardHandler.addTypes({ "Base.CatanRobber", "Base.LongestRoad", "Base.LargestArmy", "Base.CatanBoard",
-                                    "Base.PlayerCostsWhite", "Base.PlayerCostsRed", "Base.PlayerCostsOrange", "Base.PlayerCostsBlue"})
----Add special cases
-gamePieceAndBoardHandler.addSpecial("Base.CatanBoard", { category = "GameBoard" })
+---Register game pieces by type -- enables the system to display the items using custom textures found in:
+--- `Item_InPlayTextures` and `Item_OutOfPlayTextures`
+--- Note: In-Play defaults to Out of play textures. Out of play textures replaces the item's texture/icon.
+gamePieceAndBoardHandler.registerTypes({
+    "Base.CatanRobber", "Base.LongestRoad", "Base.LargestArmy", "Base.CatanBoard",
+    "Base.PlayerCostsWhite", "Base.PlayerCostsRed", "Base.PlayerCostsOrange", "Base.PlayerCostsBlue"})
+
+---Register special cases for modifying parts of the item
+--- For example, the board is registered to the game-system above - which turns it into a 'GamePiece'
+--- This `special` case changes the category from `GamePiece` to `GameBoard`.
+gamePieceAndBoardHandler.registerSpecial("Base.CatanBoard", { category = "GameBoard" })
