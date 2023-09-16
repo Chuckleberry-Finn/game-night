0a4516d15f9dcd8e3e0c33e38d63fcb9b29d0e6f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-09-16 07:23:29 -0400

    Update gameNight - gamePieceAndBoardHandler.lua

--- Contents/mods/game-night/media/lua/shared/gameNight - gamePieceAndBoardHandler.lua
+++ Contents/mods/game-night/media/lua/shared/gameNight - gamePieceAndBoardHandler.lua
@@ -142,8 +142,7 @@ function gamePieceAndBoardHandler.pickupAndPlaceGamePiece(item, square, player, 
         if not worldItemObj then return end
         if worldItemObj then oldZ = worldItemObj:getWorldPosZ()-worldItemObj:getZ() end
 
-        local transferAction = ISInventoryTransferAction:new(player, item, item:getContainer(), player:getInventory())
-        transferAction.maxTime = 1
+        local transferAction = ISInventoryTransferAction:new(player, item, item:getContainer(), player:getInventory(), 1)
         transferAction.putSoundTime = getTimestamp() + 100
         ISTimedActionQueue.add(transferAction)
 
