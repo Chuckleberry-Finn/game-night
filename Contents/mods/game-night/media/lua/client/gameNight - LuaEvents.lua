a32957f32fe97edcb55022bad548266a4db7f20e
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-09-18 19:14:49 -0400

    lua events

--- Contents/mods/game-night/media/lua/client/gameNight - LuaEvents.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - LuaEvents.lua
@@ -1,6 +1,3 @@
-local gamePieceAndBoardHandler = require "gameNight - gamePieceAndBoardHandler"
-Events.OnGameBoot.Add(gamePieceAndBoardHandler.applyScriptChanges)
-
 local applyItemDetails = require "gameNight - applyItemDetails"
 Events.OnRefreshInventoryWindowContainers.Add(applyItemDetails.applyToInventory)
 Events.OnFillContainer.Add(applyItemDetails.applyToFillContainer)
--- Contents/mods/game-night/media/lua/server/gameNight - serverLuaEvents.lua
+++ Contents/mods/game-night/media/lua/server/gameNight - serverLuaEvents.lua
@@ -1,5 +1,7 @@
 local gameNightDistro = require "gameNight - Distributions"
-
 Events.OnGameBoot.Add(gameNightDistro.addToSuburbsDist())
 Events.OnGameBoot.Add(gameNightDistro.overrideProceduralDist())
 Events.OnGameBoot.Add(gameNightDistro.fillProceduralDist())
+
+local gamePieceAndBoardHandler = require "gameNight - gamePieceAndBoardHandler"
+Events.OnGameBoot.Add(gamePieceAndBoardHandler.applyScriptChanges)
