1bcfcc545099bad02034d8aa81a074a528ab05f8
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-09-29 17:56:21 -0400

    Create gameNight - implementMonopoly.lua

---
+++ Contents/mods/game-night - Monopoly/media/lua/client/gameNight - implementMonopoly.lua
@@ -0,0 +1,37 @@
+---Purple
+--Mediterranean Avenue
+--Baltic Avenue
+
+---Blue
+--Oriental Avenue
+--Vermont Avenue
+--Connecticut Avenue
+
+---Pink
+--St. Charles Place
+--States Avenue
+--Virginia Avenue
+
+---Orange
+--St. James Place
+--Tennessee Avenue
+--New York Avenue
+
+---Red
+--Kentucky Avenue
+--Indiana Avenue
+--Illinois Avenue
+
+---Yellow
+--Atlantic Avenue
+--Ventnor Avenue
+--Marvin Gardens
+
+---Green
+--Pacific Avenue
+--North Carolina Avenue
+--Pennsylvania Avenue
+
+---Blue
+--Park Place
+--Boardwalk
