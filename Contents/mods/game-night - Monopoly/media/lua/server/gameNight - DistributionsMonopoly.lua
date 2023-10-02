4f7a96714f52df83a12f15ba75502651f49aef25
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-10-01 20:50:41 -0400

    Update gameNight - DistributionsMonopoly.lua

--- Contents/mods/game-night - Monopoly/media/lua/server/gameNight - DistributionsMonopoly.lua
+++ Contents/mods/game-night - Monopoly/media/lua/server/gameNight - DistributionsMonopoly.lua
@@ -8,6 +8,8 @@ gameNightDistro.gameNightBoxes["MonopolyBox"] = {
     rolls = 1,
     items = {
         "Dice", 9999, "Dice", 9999, "MonopolyBoard", 9999, "MonopolyDeed", 9999, "MonopolyChance", 9999, "MonopolyCommunityChest", 9999,
+        "MonopolyBoat", 9999, "MonopolyBoot", 9999, "MonopolyCar", 9999, "MonopolyDog", 9999, "MonopolyHat", 9999,
+        "MonopolyIron", 9999, "MonopolyThimble", 9999, "MonopolyWheelbarrow", 9999,
     },
     junk = { rolls = 1, items = {} }, fillRand = 0,
 }
