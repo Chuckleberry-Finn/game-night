311613e1d8d45155757ff2645521c66e81932089
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-09 19:32:05 -0500

    Update gameNight - alert.lua

--- Contents/mods/game-night/media/lua/client/gameNight - alert.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - alert.lua
@@ -1,4 +1,8 @@
-local donationSystem = require "chuckleberryFinnModdingAlertSystem"
-if not donationSystem then return end
-donationSystem.addTexture("media/textures/donate/1.png")
-donationSystem.addTexture("media/textures/donate/2.png")
+local alertSystem = require "chuckleberryFinnModdingAlertSystem"
+if not alertSystem then return end
+alertSystem.addTexture("media/textures/donate/1.png")
+alertSystem.addTexture("media/textures/donate/2.png")
+
+local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
+if modCountSystem then modCountSystem.pullAndAddModID()
+else print("ERROR: MISSING MOD: `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`)") end
