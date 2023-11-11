a252d24abcaa1ef128b727aff1d4f1eb3b85f0e0
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-11 16:31:20 -0500

    Update gameNight - alert.lua

--- Contents/mods/game-night/media/lua/client/gameNight - alert.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - alert.lua
@@ -1,8 +1,10 @@
-local alertSystem = require "chuckleberryFinnModdingAlertSystem"
-if not alertSystem then return end
-alertSystem.addTexture("media/textures/donate/1.png")
-alertSystem.addTexture("media/textures/donate/2.png")
-
 local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
 if modCountSystem then modCountSystem.pullAndAddModID()
 else print("ERROR: MISSING MOD: `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`)") end
+
+local alertSystem = require "chuckleberryFinnModdingAlertSystem"
+if alertSystem then
+    alertSystem.addTexture("media/textures/donate/1.png")
+    alertSystem.addTexture("media/textures/donate/2.png")
+    alertSystem:receiveAlert("Brand new scaling feature! Play-Game UI can now scale to 1x, 1.5x, and 2x.")
+end 
