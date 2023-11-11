1af1b21be848bb924749ee664f4152a90b8a5ecd
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-11 16:50:50 -0500

    Update gameNight - alert.lua

--- Contents/mods/game-night/media/lua/client/gameNight - alert.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - alert.lua
@@ -6,5 +6,5 @@ local alertSystem = require "chuckleberryFinnModdingAlertSystem"
 if alertSystem then
     alertSystem.addTexture("media/textures/donate/1.png")
     alertSystem.addTexture("media/textures/donate/2.png")
-    alertSystem:receiveAlert("Brand new scaling feature! Play-Game UI can now scale to 1x, 1.5x, and 2x.")
-end 
+    --alertSystem:receiveAlert("Brand new scaling feature! Play-Game UI can now scale to 1x, 1.5x, and 2x.")
+end
