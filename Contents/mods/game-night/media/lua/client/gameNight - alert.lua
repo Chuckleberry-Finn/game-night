df0749c8d319f53ed6a5551c31cd895ad702df67
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-14 18:52:40 -0500

    Update gameNight - alert.lua

--- Contents/mods/game-night/media/lua/client/gameNight - alert.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - alert.lua
@@ -1,6 +1,7 @@
-local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
-if modCountSystem then modCountSystem.pullAndAddModID()
-else print("ERROR: MISSING MOD: `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`)") end
+if getActivatedMods():contains("ChuckleberryFinnAlertSystem") then
+    local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
+    if modCountSystem then modCountSystem.pullAndAddModID() end
+else print("WARNING: Highly recommended to install `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`) for latest news and updates.") end
 
 local alertSystem = require "chuckleberryFinnModdingAlertSystem"
 if alertSystem then
