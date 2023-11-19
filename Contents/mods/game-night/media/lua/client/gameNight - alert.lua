b308a31a3c1ad24a155d492b7b584b78d7008b49
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-19 09:40:27 -0500

    Update gameNight - alert.lua

--- Contents/mods/game-night/media/lua/client/gameNight - alert.lua
+++ Contents/mods/game-night/media/lua/client/gameNight - alert.lua
@@ -1,11 +1,15 @@
 if getActivatedMods():contains("ChuckleberryFinnAlertSystem") then
+
     local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
     if modCountSystem then modCountSystem.pullAndAddModID() end
-else print("WARNING: Highly recommended to install `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`) for latest news and updates.") end
 
-local alertSystem = require "chuckleberryFinnModdingAlertSystem"
-if alertSystem then
-    alertSystem.addTexture("media/textures/donate/1.png")
-    alertSystem.addTexture("media/textures/donate/2.png")
-    --alertSystem:receiveAlert("Brand new scaling feature! Play-Game UI can now scale to 1x, 1.5x, and 2x.")
+    local alertSystem = require "chuckleberryFinnModdingAlertSystem"
+    if alertSystem then
+        alertSystem.addTexture("media/textures/donate/1.png")
+        alertSystem.addTexture("media/textures/donate/2.png")
+        --alertSystem:receiveAlert("Brand new scaling feature! Play-Game UI can now scale to 1x, 1.5x, and 2x.")
+    end
+
+else
+    print("WARNING: Highly recommended to install `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`) for latest news and updates.")
 end
