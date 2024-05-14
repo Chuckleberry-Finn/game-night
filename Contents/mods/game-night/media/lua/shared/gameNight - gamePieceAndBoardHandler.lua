ca799a3102dab8beab1d2ff00432e10be7ffc5d3
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-05-13 20:09:38 -0400

    Update gameNight - gamePieceAndBoardHandler.lua

--- Contents/mods/game-night/media/lua/shared/gameNight - gamePieceAndBoardHandler.lua
+++ Contents/mods/game-night/media/lua/shared/gameNight - gamePieceAndBoardHandler.lua
@@ -488,7 +488,8 @@ function gamePieceAndBoardHandler.examine(gamePiece, player, indexIfCard)
     local fullType = gamePiece:getFullType()
     local specialCase = fullType and gamePieceAndBoardHandler.specials[fullType]
     local examineScale = specialCase and specialCase.examineScale
-    if examineScale then gameNightExamine.open(player, gamePiece, true, indexIfCard) end
+    local examineAction = specialCase and specialCase.actions and specialCase.actions.examineCard
+    if examineScale or examineAction then gameNightExamine.open(player, gamePiece, true, indexIfCard) end
 end
 
 
