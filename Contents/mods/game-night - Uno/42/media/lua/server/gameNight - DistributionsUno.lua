55fc1867212d1e3253e80dbc603c6258824ebf2b
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-01-31 18:09:00 -0500

    uno changes

--- Contents/mods/game-night - Uno/42/media/lua/server/gameNight - DistributionsUno.lua
+++ Contents/mods/game-night - Uno/42/media/lua/server/gameNight - DistributionsUno.lua
@@ -2,13 +2,5 @@ require "Items/SuburbsDistributions"
 
 local gameNightDistro = require "gameNight - Distributions"
 
-gameNightDistro.proceduralDistGameNight.itemsToAdd["UnoBox"] = {}
-
-gameNightDistro.gameNightBoxes["UnoBox"] = {
-    rolls = 1,
-    items = {
-        "UnoCards", 9999,
-    },
-    junk = { rolls = 1, items = {} }, fillRand = 0,
-}
+gameNightDistro.proceduralDistGameNight.itemsToAdd["UnoCards"] = {}
 
